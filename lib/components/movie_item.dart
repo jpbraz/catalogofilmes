104be74cd6a03069051e6c3b964b026df36cecd9
João Paulo Araujo Braz <jparaujobraz84@gmail.com>
2022-05-08 01:32:10 -0300

    Inserida Imagem e ajustes no componente movie_item

--- lib/components/movie_item.dart
+++ lib/components/movie_item.dart
@@ -4,17 +4,90 @@ import 'package:flutter/material.dart';
 class MovieItem extends StatelessWidget {
   final Movie movie;
 
-  MovieItem(this.movie);
+  const MovieItem(this.movie, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(10),
+    String movieTitle = movie.title as String;
+    String posterPath = movie.posterPath as String;
+    String releaseDate = movie.releaseDate as String;
+    String ano = releaseDate.substring(0, 4);
+
+    return GestureDetector(
+      onTap: () {
+        print(movie.id);
+      },
+      child: Container(
+        margin: const EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          image: DecorationImage(
+            opacity: 20,
+            image: NetworkImage(
+              'https://image.tmdb.org/t/p/w220_and_h330_face$posterPath',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: const EdgeInsets.all(5),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(movie.originalTitle as String),
-            Text(movie.title as String),
+            Text(
+              movieTitle,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white70,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                shadows: [
+                  Shadow(
+                    color: Colors.black,
+                    blurRadius: 10.0,
+                    offset: Offset(-10.0, 5.0),
+                  ),
+                  Shadow(
+                    blurRadius: 10.0,
+                    color: Colors.blue,
+                    offset: Offset(5.0, 5.0),
+                  ),
+                  Shadow(
+                    color: Colors.green,
+                    blurRadius: 10.0,
+                    offset: Offset(-10.0, 5.0),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              ano,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white70,
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+                shadows: [
+                  Shadow(
+                    color: Colors.black,
+                    blurRadius: 10.0,
+                    offset: Offset(-10.0, 5.0),
+                  ),
+                  Shadow(
+                    blurRadius: 10.0,
+                    color: Colors.blue,
+                    offset: Offset(5.0, 5.0),
+                  ),
+                  Shadow(
+                    color: Colors.green,
+                    blurRadius: 10.0,
+                    offset: Offset(-10.0, 5.0),
+                  ),
+                ],
+              ),
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
