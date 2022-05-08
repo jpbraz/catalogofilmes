5520f8fb8ac0d07636e88ffe5bf2696d2c88a2b0
João Paulo Araujo Braz <jparaujobraz84@gmail.com>
2022-05-08 19:11:47 -0300

    implemented Rating and Route to Details

--- lib/components/movie_item.dart
+++ lib/components/movie_item.dart
@@ -1,5 +1,8 @@
 import 'package:catalogo_filmes/models/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+import '../screens/details_screen.dart';
 
 class MovieItem extends StatelessWidget {
   final Movie movie;
@@ -12,78 +15,88 @@ class MovieItem extends StatelessWidget {
     String posterPath = movie.posterPath as String;
     String releaseDate = movie.releaseDate as String;
     String ano = releaseDate.substring(0, 4);
+    double? rate = movie.voteAverage;
+    int percentual = (rate! * 10).toInt();
+
+    const boxShadow2 = BoxShadow(
+      offset: Offset(0, 4),
+      blurRadius: 4,
+      color: Colors.black54,
+    );
 
     return GestureDetector(
-      onTap: () {
-        print(movie.id);
-      },
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => DetailsScreen(movie),
+        ),
+      ),
       child: Container(
         margin: const EdgeInsets.all(5),
         decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          boxShadow: const [boxShadow2],
           shape: BoxShape.rectangle,
           image: DecorationImage(
             opacity: 20,
             image: NetworkImage(
               'https://image.tmdb.org/t/p/w220_and_h330_face$posterPath',
             ),
-            fit: BoxFit.cover,
+            fit: BoxFit.fill,
           ),
         ),
         padding: const EdgeInsets.all(5),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              movieTitle,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white70,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-                shadows: [
-                  Shadow(
-                    color: Colors.black,
-                    blurRadius: 10.0,
-                    offset: Offset(-10.0, 5.0),
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                children: [
+                  Text(
+                    movieTitle,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      shadows: [boxShadow2],
+                    ),
                   ),
-                  Shadow(
-                    blurRadius: 10.0,
-                    color: Colors.blue,
-                    offset: Offset(5.0, 5.0),
-                  ),
-                  Shadow(
-                    color: Colors.green,
-                    blurRadius: 10.0,
-                    offset: Offset(-10.0, 5.0),
+                  Text(
+                    ano,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white70,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      shadows: [boxShadow2],
+                    ),
                   ),
                 ],
               ),
             ),
-            Text(
-              ano,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white70,
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
-                shadows: [
-                  Shadow(
-                    color: Colors.black,
-                    blurRadius: 10.0,
-                    offset: Offset(-10.0, 5.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                CircularPercentIndicator(
+                  animation: true,
+                  rotateLinearGradient: true,
+                  radius: 20.0,
+                  lineWidth: 6.0,
+                  percent: rate / 10,
+                  center: Text(
+                    '$percentual',
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
                   ),
-                  Shadow(
-                    blurRadius: 10.0,
-                    color: Colors.blue,
-                    offset: Offset(5.0, 5.0),
-                  ),
-                  Shadow(
-                    color: Colors.green,
-                    blurRadius: 10.0,
-                    offset: Offset(-10.0, 5.0),
-                  ),
-                ],
-              ),
+                  progressColor: Colors.amber,
+                  backgroundColor: Colors.white,
+                ),
+              ],
             ),
           ],
         ),
