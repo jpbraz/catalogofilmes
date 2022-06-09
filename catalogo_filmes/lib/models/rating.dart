2665603742f6d113e70add1fbbaa31f33c0eceeb
João Paulo Araujo Braz <jparaujobraz84@gmail.com>
2022-06-09 10:36:52 -0300

    Criação da entidade de Avaliação (e Comentários).

---
+++ catalogo_filmes/lib/models/rating.dart
@@ -0,0 +1,32 @@
+/* 
+#13
+Criar entidade para avaliação do filme com os atributos:
+
+Comentário
+Nota
+Filme
+*/
+
+class Rating {
+  String id; // ID da avaliação
+  int movieId; // ID do movie selecionado
+  double value; // Nota da Avaliação
+  String? comments; // Comentários da avaliação
+
+  Rating({
+    required this.id,
+    required this.movieId,
+    required this.value,
+    this.comments,
+  });
+
+/*
+  void update(Rating rating) {
+    if (this == rating) {
+      movieId = rating.movieId;
+      value = rating.value;
+      comments = rating.comments;
+    }
+  }
+*/
+}
